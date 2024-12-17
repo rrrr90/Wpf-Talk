4aff218a7094c2e43c34b4580d557d533c8da149
rrrr <vegaruvit@gmail.com>
2024-12-17 17:09:56 +0900

    add find parent extensions

---
+++ Wpf-Lib/Extensions/FindParentExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Wpf_Lib.Extensions
+{
+    public static class FindParentExtensions
+    {
+        public static T? FindParent<T>(this DependencyObject child)
+    where T : DependencyObject
+        {
+            return FindParent<T>(child, null);
+        }
+
+
+        public static T? FindParent<T>(this DependencyObject child, string? parentName)
+            where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(child);
+            if (parent == null) return null;
+
+            var frameworkElement = parent as FrameworkElement;
+            if (parentName == null ||
+                frameworkElement is T && parentName == frameworkElement.Name) return parent as T;
+
+            return FindParent<T>(parent, parentName);
+        }
+    }
+}
