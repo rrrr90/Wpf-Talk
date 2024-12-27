2dfc0a0728b8630e5f50e6fd1fca5bb075ecc67e
rrrr <vegaruvit@gmail.com>
2024-12-27 15:41:17 +0900

    impl signin command

--- Wpf-Talk/ViewModels/MainViewModel.cs
+++ Wpf-Talk/ViewModels/MainViewModel.cs
@@ -8,13 +8,16 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
+using Wpf_Talk.Stores;
 
 namespace Wpf_Talk.ViewModels
 {
     internal partial class MainViewModel : ObservableObject
     {
         private INotifyPropertyChanged _currentViewModel;
-        public INotifyPropertyChanged CurrentViewModel
+        private readonly MainNavigationStore _mainNavigationStore;
+
+        public INotifyPropertyChanged? CurrentViewModel
         {
             get => _currentViewModel;
             set
@@ -25,9 +28,16 @@ namespace Wpf_Talk.ViewModels
             }
         }
 
-        public MainViewModel()
+        public MainViewModel(MainNavigationStore mainNavigationStore)
         {
             _currentViewModel = App.Current.Services.GetService<SignInViewModel>()!;
+            this._mainNavigationStore = mainNavigationStore;
+            _mainNavigationStore.CurrentViewModelChanged += CurrentViewModelChanged;
+        }
+
+        private void CurrentViewModelChanged()
+        {
+            CurrentViewModel = _mainNavigationStore.CurrentViewModel;
         }
 
         public ICommand ToSignUpCommand => new RelayCommand(ToSignUp);
