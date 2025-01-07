64d77c48a7a5c5c38ed80972327d10f80b6fd53e
rrrr <vegaruvit@gmail.com>
2025-01-07 22:09:18 +0900

    connect mysql

---
+++ Wpf-DB/Database/MySqlDB.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_DB.Database
+{
+    public class MySqlDB
+    {
+        private readonly string _connectionString;
+        private MySqlConnection? _connection;
+
+        public MySqlDB(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        private void Connect()
+        {
+            _connection = new MySqlConnection(_connectionString);
+            _connection.Open();
+        }
+    }
+}
---
+++ Wpf-DB/Repositories/RepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_DB.Database;
+
+namespace Wpf_DB.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        public MySqlDB GetMySqlDB()
+        {
+            return new MySqlDB(Properties.Resources.WPFDB_CONNECTION);
+        }
+    }
+}
