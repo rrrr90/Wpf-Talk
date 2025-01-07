b58ef20256f3f9b51721a6395dae2c252270fb2e
rrrr <vegaruvit@gmail.com>
2025-01-07 22:24:57 +0900

    add functions to process Insert query

--- Wpf-DB/Database/MySqlDB.cs
+++ Wpf-DB/Database/MySqlDB.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,35 @@ namespace Wpf_DB.Database
             }
             cmd.ExecuteNonQuery();
             return cmd.LastInsertedId;
+        }
+
+        public IDataReader GetReader(string query, SqlParameter[]? parms = null)
+        {
+            using MySqlCommand cmd = new MySqlCommand(query, _connection);
+            if (parms != null)
+            {
+                foreach (SqlParameter parm in parms)
+                {
+                    cmd.Parameters.AddWithValue(parm.ParameterName, parm.Value);
+                }
+            }
+            return cmd.ExecuteReader();
+        }
+
+        public DataTable GetDataTable(string query, SqlParameter[]? parms = null)
+        {
+            using MySqlCommand cmd = new MySqlCommand(query, _connection);
+            if (parms != null)
+            {
+                foreach (SqlParameter parm in parms)
+                {
+                    cmd.Parameters.AddWithValue(parm.ParameterName, parm.Value);
+                }
+            }
+            using MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            return table;
         }
     }
 }
