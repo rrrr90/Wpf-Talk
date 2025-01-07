bc95b4baf8e4a6ef55e5dd9a91459a64d94a1c81
rrrr <vegaruvit@gmail.com>
2025-01-07 23:13:08 +0900

    create account repository

---
+++ Wpf-DB/Model/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_DB.Model
+{
+    public class Account
+    {
+        public int ID { get; set; } = default(int);
+        public string Email {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Nickname { get; set; } = string.Empty;
+        public string CellPhone { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"ID:{ID}, Email:{Email}, Pwd:{Password}, Nickname:{Nickname}, Phone:{CellPhone}";
+        }
+    }
+}
