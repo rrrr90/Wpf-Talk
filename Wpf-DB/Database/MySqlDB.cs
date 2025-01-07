b6a6f7f40f12ba6096bd1fba9d22cb92ab22a458
rrrr <vegaruvit@gmail.com>
2025-01-07 22:20:06 +0900

    create base of execute function

--- Wpf-DB/Database/MySqlDB.cs
+++ Wpf-DB/Database/MySqlDB.cs
@@ -15,12 +15,27 @@ namespace Wpf_DB.Database
         public MySqlDB(string connectionString)
         {
             this._connectionString = connectionString;
+            Connect();
         }
 
         private void Connect()
         {
             _connection = new MySqlConnection(_connectionString);
             _connection.Open();
+        }
+
+        public long Execute(string query, SqlParameter[]? parms = null)
+        {
+            using MySqlCommand cmd = new MySqlCommand(query, _connection);
+            if (parms != null)
+            {
+                foreach (SqlParameter parm in parms)
+                {
+                    cmd.Parameters.AddWithValue(parm.ParameterName, parm.Value);
+                }
+            }
+            cmd.ExecuteNonQuery();
+            return cmd.LastInsertedId;
         }
     }
 }
---
+++ Wpf-DB/Database/SqlParameter.cs
@@ -0,0 +1,17 @@
+﻿namespace Wpf_DB.Database
+{
+    public class SqlParameter
+    {
+        private string _parameterName;
+        private object _value;
+
+        public string ParameterName => _parameterName;
+        public object Value => _value;
+
+        public SqlParameter(string parameterName, object value)
+        {
+            _parameterName = parameterName;
+            _value = value;
+        }
+    }
+}
